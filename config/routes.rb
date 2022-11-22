2197b072b10e5dde5bc777393cb4b4f29546a10f
Romain Heranney <heranney.romain@gmail.com>
2022-11-22 12:36:28 +0100

    functionnal create link to a booking

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :gaming_sessions, only: %i[index show new create] do
     resources :bookings, only: %i[create]
   end
-
+  get 'dashboard', to: 'pages#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
