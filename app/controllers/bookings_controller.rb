86a049a6b663a9359bc15f7003ee899ed985c273
Romain Heranney <heranney.romain@gmail.com>
2022-11-22 11:51:15 +0100

    end of model_booking update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,8 +2,10 @@ class BookingsController < ApplicationController
   before_action :set_gaming_session, only: %i[create]
 
   def create
+    # @user = User.find(params[:user_id])
     @booking = Booking.new(booking_params)
     @booking.gaming_session = @gaming_session
+    # @booking.user = @user
     if @booking.save
       redirect_to gaming_session_path(@gaming_session)
     else
