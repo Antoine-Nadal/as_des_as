c9446f1308558c3634d5cbc70ec5544c7ac98480
Fabrizio Durand <fabrizio.durand9106@gmail.com>
2022-11-23 11:53:57 +0100

    added geocoding to the project. Map is displayed on the gamingsessions/index. Autocomplete form for gamingsessions address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_144617) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_094331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_144617) do
     t.string "name"
     t.text "description"
     t.integer "price"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_gaming_sessions_on_user_id"
   end
 
